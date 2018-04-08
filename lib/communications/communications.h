95a1b41ffa5f9efc2365bc3f072d47f62ddcb3e5
AndrewBiscuit <andrewalford1@outlook.com>
2018-04-08 19:51:36 +0100

    Added in communications stuff

---
+++ lib/communications/communications.h
@@ -0,0 +1,1 @@
+#include "reciever/Reciever.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,25 @@
 #include <mbed.h>
 #include <EthernetInterface.h>
 #include <rtos.h>
-
+#include <string>
 #include <C12832.h>
-char buffer[1024]; /* 1k bytes */
+#include "communications.h"
 
-int main() {
+int main()
+{
     C12832 lcd(D11, D13, D12, D7, D10);
-    EthernetInterface eth;
-    UDPSocket udp;
 
-    printf("conecting on DHCP\n");
-    eth.connect();
-    const char *ip = eth.get_ip_address();
-    printf("IP address is: %s\n", ip ? ip : "No IP");
+    Reciever reciever;
 
-    udp.open( &eth);
+    while(1)
+    {
+        string message = reciever.getMessage();
 
-    /* Listen for UDP on port 65000 */
-    udp.bind(80);
-
-    printf("listening on 65000\n");
-
-    while(1){
-        SocketAddress source;
-        int len = udp.recvfrom( &source, buffer, sizeof(buffer));
-        buffer[len]='\0';
-
-        printf("message from %s/%d \n", source.get_ip_address(), source.get_port());
-        lcd.locate(0,0);
-        lcd.printf("\"%s\"", buffer);
+        if(!message.empty())
+        {
+            lcd.cls();
+            lcd.locate(0,0);
+            lcd.printf("\"%s\"", message.c_str());
+        }
     }
 }
